3a2197dd54581dd3679a970150aa60fd97614bf3
pardeep kesnani <pardeep.kesnani@live.com>
2019-04-25 00:23:04 +0500

    java projects
    this folder contain java projects that i have done in university

---
+++ java project/Application/src/javax/swing/JOptionpane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javax.swing;
+
+/**
+ *
+ * @author pardeep
+ */
+public class JOptionpane {
+
+    public static void showMessageDialog(Object object, String its_owesome_try_it_again) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
